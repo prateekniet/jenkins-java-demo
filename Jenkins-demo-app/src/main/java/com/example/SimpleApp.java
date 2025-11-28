2a7b883b6573b8f3c727366ecfb512f96110d1aa
scaler-bhavya <bhavya.sachdeva@scaler.com>
2025-11-28 13:08:40 +0530

    Update SimpleApp.java

--- Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
+++ Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
@@ -3,7 +3,7 @@ package com.example;
 public class SimpleApp {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------");
-        System.out.println("Hello! This application was built via Jenkins.Version-3");
+        System.out.println("Hello! This application was built via Jenkins.Version-4");
         System.out.println("--------------------------------------------------");
     }
 }
