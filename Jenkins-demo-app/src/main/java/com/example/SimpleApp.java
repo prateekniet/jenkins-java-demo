edf98d379f4b4752668f503c80d1ca39ee63206d
Ubuntu <ubuntu@ip-172-31-18-2.us-west-2.compute.internal>
2025-11-28 06:51:54 +0000

    check

---
+++ Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("Hello! This application was built via Jenkins.");
+        System.out.println("--------------------------------------------------");
+    }
+}
