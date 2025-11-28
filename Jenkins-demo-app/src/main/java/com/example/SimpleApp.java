eadaf3a7762b6f50392af5d7d8af7cce335654c2
prateekniet <prateekniet35@gmail.com>
2025-11-28 22:45:30 +0530

    Update SimpleApp.java

--- Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
+++ Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
@@ -4,6 +4,6 @@ public class SimpleApp {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------");
         System.out.println("Hello! This application was built via Jenkins.Version-5");
-        System.out.println("--------------------------------------------------"); ??????
+        System.out.println("--------------------------------------------------");
     }
 }
